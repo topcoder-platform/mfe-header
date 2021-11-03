b8eead5de2d11c807b2db09803b0ed5d8a99341a
Rakib Ansary <rakibansary@gmail.com>
2021-11-03 16:27:52 +0600

    chore: update config

--- config/dev.js
+++ config/dev.js
@@ -12,5 +12,5 @@ module.exports = {
     V5: "https://api.topcoder-dev.com/v5",
   },
   REAUTH_OFFSET: 55, // seconds
-  PROFILE_CREATION_DATE_THRESHOLD: "2019-01-01", // format: "YYYY-MM-DD"
+  PROFILE_CREATION_DATE_THRESHOLD: "2021-10-25", // format: "YYYY-MM-DD"
 };
